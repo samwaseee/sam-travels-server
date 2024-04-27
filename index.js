c2c3f75086c41b783970b79e7a00ae498721e2c8
samwaseee <samwaseee@gmail.com>
2024-04-27 08:59:09 +0600

    POST for touristSpot added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//mongodb+srv://Sam-travels-A10:VmuDt4rv3jsUAT7B@cluster0.ti5xab5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ti5xab5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -26,6 +27,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const touristSpotCollection = client.db('SAMtravelsDB').collection('touristSpot');
+
+    app.get('/touristSpot', async(req,res)=>{
+      const cursor = touristSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+    app.post('/touristSpot', async(req,res) =>{
+      const newSpot = req.body;
+      console.log(newSpot);
+      const result = await touristSpotCollection.insertOne(newSpot);
+      res.send(result);
+  })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
